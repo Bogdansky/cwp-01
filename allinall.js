71de865f7e269fa9191319aa08e87e09771f8e06
Bogdan <bogdya2@gmail.com>
2018-09-18 22:12:53 +0300

    just in case

--- allinall.js
+++ allinall.js
@@ -1,54 +1,51 @@
 let fs = require('fs');
 let path = require('path');
-let copyright = '{"author": "Bogdan Incorporated", "symbol": "©2018. All rights reserved."}';
-
-const directory = "E:\\Учёба"; //"E:\\Учёба\\ПСКП\\Первая лабораторная\\cwp-01"
-console.log(directory);
-const dirname = directory +"\\texts";
-if (!fs.existsSync(dirname)){
-    fs.mkdir( dirname, function (err) {
-        if (err) throw err;
-        console.log("Directory was created!" )
-    });
-}
-
-fs.writeFile(dirname+"\\config.json", copyright, (err) => {
-    if (err) throw err;
-    console.log("config.json was created!")
-});
-
-let getFiles = function (dir, files_){
-
-    files_ = files_ || [];
-    fs.readdir(dir, (err, files) => {
-        for (let i in files){
-        let name = dir+'\\' + files[i];
-        let filePath = dirname+"\\"+ path.win32.basename(name);
-        if (fs.statSync(name).isDirectory()){
-            if (name === dirname) continue;
-            getFiles(name, files_);
-        } else {
-            if (path.extname(name) === ".txt"){
-                fs.copyFile(name,filePath, (err) => {
-                        fs.writeFile(filePath, JSON.stringify(copyright), 0, (err) => {
-                            if (err) throw err;
-                            fs.appendFile(filePath, copyright, (err) => {
-                                if (err) throw err;
-                            });
-                        });
-                        fs.watch(filePath, (event, file) => {
-                           if (event === 'change'){
-                               console.log(file+" changed");
-                           } 
-                        });
-                    });
-                    console.log(path.win32.basename(name)+" was copied in "+directory+"\\texts");
-            }
-            files_.push(name.substr(directory.length+2));
-        }
-        }
-    });
-    return files_;
-};
-
-console.log(getFiles(directory));
+ let copyright = {"author" : "Bogdan Incorporated" , "symbol" : "@2018. All rights reserved." };
+      
+ const directory = "e:\\Учёба" ;
+ const dirname = directory +"\\texts" ;
+ if (!fs.existsSync(dirname)){     
+     fs.mkdir( dirname, function (err) {     
+         //if (err) throw err;     
+         console.log("Directory was created!");
+     });     
+ }     
+      
+ fs.writeFile(dirname+"\\config.json", copyright, (err) => {
+     //if (err) throw err;     
+     console.log("config.json was created!" );
+ });     
+      
+ let getFiles = function (dir, files_){     
+      
+     files_ = files_ || [];    
+     fs.readdir(dir, (err, files) => {     
+         for (let i in files){     
+         let name = dir +'\\'+files[i];
+         let filePath = dirname +"\\" + path.win32.basename(name);
+         if (fs.statSync(name).isDirectory()){     
+             if (name === dirname) continue;     
+             getFiles(name,files_);     
+         } else {     
+             if (path.extname(name) === ".txt" ){
+                 fs.readFile(name, (err, data) => {     
+                     fs.writeFile(filePath, copyright  +   (data == null ? ""  : data)  +   copyright, (err)=>{
+                         if (err) throw err;     
+                         fs.watch(filePath, (event, file) => {     
+                             if (event === 'change'){     
+                                 console.log(file + " changed");
+                             }     
+                         });     
+                     });     
+                 });     
+                 console.log(path.win32.basename(name)+" was copied in "+directory+"\\texts" );
+                 files_.push(name.substr(directory.length+2));
+             }     
+                  
+         }     
+         }     
+     });     
+ return files_;    
+ };     
+      
+ console.log(getFiles(directory));
--- krutoy.js
+++ krutoy.js
@@ -1,25 +1,63 @@
 const directory = process.argv[2];
-if (process.argv.length > 3)
-let code = "let directory = \""+directory+"\";"+
-    "let fs = require(\"fs\");" +
-    "fs.readdir(directory, function(error, items){" +
-    "items.forEach(function(item){" +
-    "console.log(item);" +
-    "});" +
-    "});"+
-    "fs.mkdir(directory+\"\\texts\", function (err) {"+
-    "if (err)throw err;"+
-    "console.log(\"Directory was created!\")"+
-    "});";
-
+const code =  "let fs = require('fs');\n" +
+    "let path = require('path');\n" +
+    " let copyright = {\"author\" : \"Bogdan Incorporated\" , \"symbol\" : \"@2018. All rights reserved.\" };     \n" +
+    "      \n" +
+    " const directory = \"e:\\Учёба\" ;    \n" +
+    " const dirname = directory +\"\\\\texts\" ;     \n" +
+    " if (!fs.existsSync(dirname)){     \n" +
+    "     fs.mkdir( dirname, function (err) {     \n" +
+    "         //if (err) throw err;     \n" +
+    "         console.log(\"Directory was created!\");     \n" +
+    "     });     \n" +
+    " }     \n" +
+    "      \n" +
+    " fs.writeFile(dirname+\"\\\\\"+ config.json , copyright, (err) => {     \n" +
+    "     //if (err) throw err;     \n" +
+    "     console.log(\"config.json was created!\" );     \n" +
+    " });     \n" +
+    "      \n" +
+    " let getFiles = function (dir, files_){     \n" +
+    "      \n" +
+    "     files_ = files_ || [];    \n" +
+    "     fs.readdir(dir, (err, files) => {     \n" +
+    "         for (let i in files){     \n" +
+    "         let name = dir +'\\\\'+files[i];     \n" +
+    "         let filePath = dirname +\"\\\\\" + path.win32.basename(name);     \n" +
+    "         if (fs.statSync(name).isDirectory()){     \n" +
+    "             if (name === dirname) continue;     \n" +
+    "             getFiles(name,files_);     \n" +
+    "         } else {     \n" +
+    "             if (path.extname(name) === \".txt\" ){     \n" +
+    "                 fs.readFile(name, (err, data) => {     \n" +
+    "                     fs.writeFile(filePath, copyright  +   (data == null ? \"\"  : data)  +   copyright, (err)=>{     \n" +
+    "                         if (err) throw err;     \n" +
+    "                         fs.watch(filePath, (event, file) => {     \n" +
+    "                             if (event === 'change'){     \n" +
+    "                                 console.log(file + \" changed\");     \n" +
+    "                             }     \n" +
+    "                         });     \n" +
+    "                     });     \n" +
+    "                 });     \n" +
+    "                 console.log(path.win32.basename(name)+\" was copied in \"+directory+\"\\\\texts\" );    \n" +
+    "                 files_.push(name.substr(directory.length+2));    \n" +
+    "             }     \n" +
+    "                  \n" +
+    "         }     \n" +
+    "         }     \n" +
+    "     });     \n" +
+    " return files_;    \n" +
+    " };     \n" +
+    "      \n" +
+    " console.log(getFiles(directory));";
 let fs = require("fs");
 let exist = fs.existsSync(directory);
-if (exist){
-    fs.writeFile(directory+"\\summary.js", code, (err)=>{
+if (exist ){
+    fs.writeFile(directory+ "\\summary.js", code, (err)=>{
         if (err) throw err;
-        console.log("Script was created by.");
+        console.log( "Script was created by. ");
     });
 }
 else {
-    console.log("Директория не существует!");
+    console.log( "Директория не существует!");
 }
